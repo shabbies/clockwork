0e3b9b5e3984008d87baa98b9a4e93feb26ad242
Kenneth <kssc91@hotmail.com>
2015-07-26 14:22:05 +0800

    facebook login progress 17

--- src/main/java/servlet/FacebookLoginServlet.java
+++ src/main/java/servlet/FacebookLoginServlet.java
@@ -1,7 +1,13 @@
 package servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import controller.PostController;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Type;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,15 +23,29 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpSession;
+import model.Post;
 import model.User;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.message.BasicNameValuePair;
 
 public class FacebookLoginServlet extends HttpServlet {
+
+    private String appID = null;
+    private String appSecret = null;
+    
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException{
+        appID = servletConfig.getInitParameter("facebookAppID");
+        appSecret = servletConfig.getInitParameter("facebookAppSecret");
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -33,9 +53,50 @@ public class FacebookLoginServlet extends HttpServlet {
         
         String accessToken = request.getParameter("access_token");
         String userID = request.getParameter("user_id");
-        System.out.println("BYE");
-        System.out.println(accessToken);
-        System.out.println(userID);
+        
+        // Checking validity of accessToken
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://graph.facebook.com/debug_token?input_token=" + accessToken + "&access_token=" + appID + "|" + appSecret);
+        CloseableHttpResponse httpResponse = httpclient.execute(httpGet);
+        try {
+            HttpEntity entity = httpResponse.getEntity();
+            StringWriter writer = new StringWriter();
+            InputStream readingStream = entity.getContent();
+            IOUtils.copy(readingStream, writer, "UTF-8");
+            String responseString = writer.toString();
+            Gson gson = new Gson();
+            Type hashType = new TypeToken<HashMap<String, Object>>(){}.getType();
+            HashMap fullHash = gson.fromJson(responseString, hashType);
+            HashMap dataHash = gson.fromJson((String)fullHash.get("data"), hashType);
+            String retrievedAppID = (String)dataHash.get("app_id");
+            String retrievedUserID = (String)dataHash.get("user_id");
+            if (!retrievedAppID.equals(appID) || !retrievedUserID.equals(userID)){
+                response.sendRedirect("/index.jsp");
+                return;
+            }
+            EntityUtils.consume(entity);
+        } finally {
+            httpResponse.close();
+        }
+        
+        // Grabbing user profile from FB
+        httpGet = new HttpGet("https://graph.facebook.com/me?" + accessToken + "&access_token=" + appID + "|" + appSecret);
+        httpResponse = httpclient.execute(httpGet);
+        try {
+            HttpEntity entity = httpResponse.getEntity();
+            StringWriter writer = new StringWriter();
+            InputStream readingStream = entity.getContent();
+            IOUtils.copy(readingStream, writer, "UTF-8");
+            String responseString = writer.toString();
+            Gson gson = new Gson();
+            Type hashType = new TypeToken<HashMap<String, Object>>(){}.getType();
+            HashMap fullHash = gson.fromJson(responseString, hashType);
+            System.out.println((String)fullHash.get("email"));
+            
+            EntityUtils.consume(entity);
+        } finally {
+            httpResponse.close();
+        }
     }
 
 }
