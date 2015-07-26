5168abcc8fbec394898ad62b081b974f428bd342
Kenneth <kssc91@hotmail.com>
2015-07-26 14:30:19 +0800

    facebook login progress 18

--- src/main/java/servlet/FacebookLoginServlet.java
+++ src/main/java/servlet/FacebookLoginServlet.java
@@ -56,7 +56,7 @@ public class FacebookLoginServlet extends HttpServlet {
         
         // Checking validity of accessToken
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://graph.facebook.com/debug_token?input_token=" + accessToken + "&access_token=" + appID + "|" + appSecret);
+        HttpGet httpGet = new HttpGet("https://graph.facebook.com/debug_token?input_token=" + accessToken + "&access_token=" + appID + "\\|" + appSecret);
         CloseableHttpResponse httpResponse = httpclient.execute(httpGet);
         try {
             HttpEntity entity = httpResponse.getEntity();
