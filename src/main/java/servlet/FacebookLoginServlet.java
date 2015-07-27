74bcd056325a84b31b14b0162f729653a74373b4
Kenneth <kssc91@hotmail.com>
2015-07-27 13:20:51 +0800

    facebook login progress 28

--- src/main/java/servlet/FacebookLoginServlet.java
+++ src/main/java/servlet/FacebookLoginServlet.java
@@ -59,8 +59,10 @@ public class FacebookLoginServlet extends HttpServlet {
             String retrievedAppID = ((Double)dataHash.get("app_id")).toString();
             String retrievedUserID = ((Double)dataHash.get("user_id")).toString();
             if (!retrievedAppID.equals(appID) || !retrievedUserID.equals(userID)){
-                System.out.println(!retrievedAppID.equals(appID));
-                System.out.println(!retrievedUserID.equals(userID));
+                System.out.println(retrievedAppID);
+                System.out.println(appID);
+                System.out.println(retrievedUserID);
+                System.out.println(userID);
                 response.sendRedirect("/index.jsp");
                 return;
             }
