4e3ac869a54c1b501cc779be44752f0a8e9741cf
Kenneth <kssc91@hotmail.com>
2015-08-06 15:27:47 +0800

    debugged facebook login

--- src/main/java/servlet/RegisterAccountServlet.java
+++ src/main/java/servlet/RegisterAccountServlet.java
@@ -79,10 +79,13 @@ public class RegisterAccountServlet extends HttpServlet {
         session.setAttribute("currentUser", user);
         
         // Redirection based on initial location
-        if (session.getAttribute("loginSource") != null && session.getAttribute("loginSource").equals("create_new_post")){
-            session.removeAttribute("loginSource");
-            response.sendRedirect("/create_post.jsp");
-            return;
+        String loginSource = (String)session.getAttribute("loginSource");
+        if (loginSource != null){
+            if (loginSource.equals("create_new_post")){
+                session.removeAttribute("loginSource");
+                response.sendRedirect("/create_post.jsp");
+                return;
+            }
         }
         response.sendRedirect("/index.jsp");
     }
