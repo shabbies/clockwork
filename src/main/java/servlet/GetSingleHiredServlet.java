59e164b2876eaad2f083d072edf06470e0ceb177
Kenneth <kssc91@hotmail.com>
2015-08-13 11:42:19 +0800

    added employer dashboard listings

--- src/main/java/servlet/GetSingleHiredServlet.java
+++ src/main/java/servlet/GetSingleHiredServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
+import controller.AppController;
+import controller.UserController;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,52 +15,63 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import model.User;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.message.BasicNameValuePair;
 
-public class HireUserServlet extends HttpServlet {
+public class GetSingleHiredServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        int postID = Integer.parseInt(request.getParameter("post_id"));
-        int applicantID = Integer.parseInt(request.getParameter("applicant_id"));
+        // preparing variables
         HttpSession session = request.getSession();
         User currentUser = (User)session.getAttribute("currentUser");
-        
         String email = currentUser.getEmail();
         String token = currentUser.getAuthenticationToken();
+        String postID = request.getParameter("id");
+        AppController appController = (AppController)session.getAttribute("appController");
+        UserController userController = appController.getUserController();
         
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("https://clockwork-api.herokuapp.com/api/v1/users/hire");
+        HttpPost httpPost = new HttpPost("https://clockwork-api.herokuapp.com/api/v1/posts/get_hired");
         httpPost.setHeader("Authentication-Token", token);
         List <NameValuePair> nvps = new ArrayList <NameValuePair>();
-        nvps.add(new BasicNameValuePair("job_id", "" + postID));
         nvps.add(new BasicNameValuePair("email", email));
-        nvps.add(new BasicNameValuePair("applicant_id", "" + applicantID));
-
+        nvps.add(new BasicNameValuePair("job_id", postID));
         httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+        
         CloseableHttpResponse httpResponse = httpclient.execute(httpPost);
         HttpEntity entity = null;
+        ArrayList <User> hiredList = null;
         try {
             entity = httpResponse.getEntity();
             if(httpResponse.getStatusLine().getStatusCode() == 201){
-                String message = "You have successfully hired!";
-                session.setAttribute("message", message);
-                response.sendRedirect("/GetSingleHiredServlet?id=" + postID);
+                StringWriter writer = new StringWriter();
+                InputStream readingStream = entity.getContent();
+                IOUtils.copy(readingStream, writer, "UTF-8");
+                String theString = writer.toString();
+                hiredList = userController.createUsersFromJSONArray(theString);
+            } else {
+                String error = "A system error has occurred, please try again";
+                session.setAttribute("error", error);
+                response.sendRedirect("/index.jsp");
                 return;
-            } 
+            }
         } finally {
-            httpResponse.close();
             EntityUtils.consume(entity);
+            httpResponse.close();
         }
+        session.setAttribute("hiredList", hiredList);
+        response.sendRedirect("/listing.jsp?id=" + postID);
     }
 
 }
