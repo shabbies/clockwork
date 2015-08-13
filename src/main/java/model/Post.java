c1534c74c15b4c3c8847e65fe1e60a7c424726e0
Kenneth <kssc91@hotmail.com>
2015-08-13 18:33:43 +0800

    debug job post initial date

--- src/main/java/model/Post.java
+++ src/main/java/model/Post.java
@@ -2,9 +2,7 @@ package model;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 
 public class Post {
     
